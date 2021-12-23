d6dcdb74971e6f5024cd75d29836b1040fef5897
Markson <zhen1090755751@gmail.com>
2021-12-23 20:10:25 +0800

    Add GitHub Action (with MORE BUGS)

--- updater.ps1
+++ updater.ps1
@@ -1,18 +1,21 @@
 # NOTE: Use this PowerShell script in PowerShell Core BUT NOT Windows PowerShell!
 
+git clone https://github.com/v2rayA/v2raya-scoop $env:temp/v2raya-scoop
+Set-Location -Path $env:temp/v2raya-scoop
+
+$v2rayaJSON = Get-Item -LiteralPath ./bucket/v2raya.json | ForEach-Object  -Process { $_.FullName }
 $version = curl --silent "https://api.github.com/repos/v2raya/v2raya/releases/latest" | Select-String -Pattern "tag_name" | ForEach-Object { ([string]$_).Split('v')[1] } |  ForEach-Object { ([string]$_).Split('"')[0] }
-$old_version = Get-Content ./bucket/v2raya.json | Select-String '"version"' | ForEach-Object { ([string]$_).Split(':')[1] } | ForEach-Object { ([string]$_).Split(',')[0] } | ForEach-Object { ([string]$_).Split('"')[1] }
+$old_version = Get-Content $v2rayaJSON | Select-String '"version"' | ForEach-Object { ([string]$_).Split(':')[1] } | ForEach-Object { ([string]$_).Split(',')[0] } | ForEach-Object { ([string]$_).Split('"')[1] }
 
 if ($version -eq $old_version) {
     Write-Host "You have latest v2rayA!"
 }
 else {
     Write-Host "Update v2rayA scoop bucket to version $version..."
-    $v2rayaJSON = Get-Item -LiteralPath ./bucket/v2raya.json | ForEach-Object  -Process { $_.FullName }
-    $old_hash = Get-Content ./bucket/v2raya.json | Select-String '"hash"'  | ForEach-Object { ([string]$_).Split(':')[1] } | ForEach-Object { ([string]$_).Split(',')[0] } | ForEach-Object { ([string]$_).Split('"')[1] }
+    $old_hash = Get-Content $v2rayaJSON | Select-String '"hash"'  | ForEach-Object { ([string]$_).Split(':')[1] } | ForEach-Object { ([string]$_).Split(',')[0] } | ForEach-Object { ([string]$_).Split('"')[1] }
     $url = "https://github.com/v2rayA/v2rayA/releases/download/v$version/v2raya_windows_x64_$version.exe"
-    $old_url = Get-Content ./bucket/v2raya.json | Select-String '"url"'  | ForEach-Object { ([string]$_).Split('"')[3] }
-    curl -L $url --output "$env:temp/v2raya_$version.exe"
+    $old_url = Get-Content $v2rayaJSON | Select-String '"url"'  | ForEach-Object { ([string]$_).Split('"')[3] }
+    curl --location $url --output "$env:temp/v2raya_$version.exe"
     $hash = Get-FileHash $env:temp/v2raya_$version.exe | Select-Object Hash | ForEach-Object -Process { $_.hash }
     (Get-Content $v2rayaJSON) -replace $old_version, $version | out-file $v2rayaJSON
     (Get-Content $v2rayaJSON) -replace $old_hash, $hash | out-file $v2rayaJSON
