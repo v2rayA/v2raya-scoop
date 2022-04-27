f2cd48f5e753d3f43e71ed866f0db8cdc390009a
Markson <marksonho@2333ma.top>
2022-04-27 17:21:50 +0800

    Use Invoke-WebRequest to visit GitHub API

--- updater.ps1
+++ updater.ps1
@@ -9,22 +9,23 @@ git config --local user.name "github-actions[bot]"
 
 
 # Update v2rayA
+$url_v2raya_releases = https://api.github.com/repos/v2rayA/v2rayA/releases/latest
 $v2rayaJSON = Get-Item -LiteralPath ./bucket/v2raya.json | ForEach-Object  -Process { $_.FullName }
-$version = curl --silent "https://api.github.com/repos/v2raya/v2raya/releases/latest" | Select-String -Pattern "tag_name" | ForEach-Object { ([string]$_).Split('v')[1] } |  ForEach-Object { ([string]$_).Split('"')[0] }
-$old_version = Get-Content $v2rayaJSON | Select-String '"version"' | ForEach-Object { ([string]$_).Split(':')[1] } | ForEach-Object { ([string]$_).Split(',')[0] } | ForEach-Object { ([string]$_).Split('"')[1] }
+$version = Invoke-WebRequest -Uri $url_v2raya_releases | ConvertFrom-Json | Select-Object tag_name | ForEach-Object { ([string]$_.tag_name).Split('v')[1] }
+$old_version = Get-Content .\bucket\v2raya.json | ConvertFrom-Json | Select-Object version | ForEach-Object { ($_.version)}
 
 if ($version -eq $old_version) {
-    Write-Host "You have latest v2rayA!"
+    Write-Host "You have latest v2rayA!"    
 }
 else {
     Write-Host "Update v2rayA scoop bucket to version $version..."
     $old_hash = Get-Content $v2rayaJSON | Select-String '"hash"'  | ForEach-Object { ([string]$_).Split(':')[1] } | ForEach-Object { ([string]$_).Split(',')[0] } | ForEach-Object { ([string]$_).Split('"')[1] }
-    $url = "https://github.com/v2rayA/v2rayA/releases/download/v$version/v2raya_windows_x64_$version.exe"
-    $old_url = Get-Content $v2rayaJSON | Select-String '"url"'  | ForEach-Object { ([string]$_).Split('"')[3] }
-    curl --location $url --output "$env:HOME/v2raya-temp/v2raya_$version.exe"
+    $url_new_v2raya = "https://github.com/v2rayA/v2rayA/releases/download/v$version/v2raya_windows_x64_$version.exe"
+    $old_url_v2raya = Get-Content $v2rayaJSON | Select-String '"url"' | ForEach-Object { ([string]$_).Split('"')[3] }
+    Invoke-WebRequest -Uri $url_new_v2raya -OutFile "$env:HOME/v2raya-temp/v2raya_$version.exe"
     $hash = Get-FileHash "$env:HOME/v2raya-temp/v2raya_$version.exe" | Select-Object Hash | ForEach-Object -Process { $_.hash }
     (Get-Content $v2rayaJSON) -replace $old_hash, $hash | out-file $v2rayaJSON
-    (Get-Content $v2rayaJSON) -replace $old_url, $url | out-file $v2rayaJSON
+    (Get-Content $v2rayaJSON) -replace $old_url_v2raya, $url_new_v2raya | out-file $v2rayaJSON
     (Get-Content $v2rayaJSON) -replace $old_version, $version | out-file $v2rayaJSON
     Write-Host "v2rayA has been updated to version $version!"
     git commit $v2rayaJSON -m "v2rayA: Update to version $version"
@@ -32,8 +33,9 @@ else {
 
 
 # Update extra v2ray rules data
+$url_data = https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest
 $DataJson = Get-Item -LiteralPath "./bucket/v2ray-rules-dat.json" | ForEach-Object  -Process { $_.FullName }
-$DataNewVersion = curl --silent "https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest" | Select-String -Pattern "tag_name" | ForEach-Object { ([string]$_).Split('"')[3] }
+$DataNewVersion = Invoke-WebRequest -Uri $url_data | ConvertFrom-Json | Select-Object "tag_name" | ForEach-Object { ([string]$_.tag_name) }
 $DataOldVersion = Get-Content $DataJson | Select-String '"version"' | ForEach-Object { ([string]$_).Split(':')[1] } | ForEach-Object { ([string]$_).Split(',')[0] } | ForEach-Object { ([string]$_).Split('"')[1] }
 
 if ($DataNewVersion -eq $DataOldVersion) {
@@ -52,12 +54,12 @@ else {
 
 
 # Update v2rayA git version
-$LatestSHA = curl -s https://api.github.com/repos/v2raya/v2raya/commits/master | Select-String "sha" | Select-Object -First 1 |  ForEach-Object { ([string]$_).Split('"')[3] }
+$LatestSHA = Invoke-WebRequest -Uri https://api.github.com/repos/v2raya/v2raya/commits/master | ConvertFrom-Json | Select-Object "sha" | ForEach-Object { ($_.sha) }
 $JSONSHA = Get-Content ./bucket/v2raya-git.json | Select-String commit_sha |  ForEach-Object { ([string]$_).Split('"')[3] }
 $RunnerPath = Get-Item -LiteralPath ./ |ForEach-Object -Process { $_.FullName }
 $version_localjson = Get-Content ".\bucket\v2raya-git.json" | Select-String version | Select-Object -First 1| ForEach-Object { ([string]$_).Split('"')[3] }
 if ($LatestSHA -eq $JSONSHA) {
-    Write-Host "毛大鹅 didn't update v2rayA anymore!"
+    Write-Host "These is no update!"
 }else {
     git clone https://github.com/v2raya/v2raya/ $HOME/v2rayA
     Set-Location -Path $HOME/v2rayA
