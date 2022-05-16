5f3e24ee0057bb1d59936f0aa1176799fc69b10a
Ryanjiena <43134606+Ryanjiena@users.noreply.github.com>
2022-05-16 22:36:02 +0800

    chore: modify '$upstream'

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "v2rayA/v2raya-scoop:main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
