f9a5eb52113a80364eb15f75ae23b207b120be2d
Markson <markson@2333ma.top>
2022-12-20 18:19:00 +0800

    Should not use --wait in start

--- scripts/v2raya/reset-passwd-v2raya.ps1
+++ scripts/v2raya/reset-passwd-v2raya.ps1
@@ -4,5 +4,5 @@ $resetPWOptions = @("--lite", "--reset-password")
 Start-Process "$(scoop prefix v2raya)\v2rayaWin.exe" -ArgumentList $resetPWOptions -WorkingDirectory "$env:TEMP" -WindowStyle Hidden -Wait
 
 $startOptions = @("--lite", "--log-disable-timestamp", "--log-file v2raya.log")
-Start-Process "$(scoop prefix v2raya)\v2rayaWin.exe" -ArgumentList $startOptions -WorkingDirectory "$env:TEMP" -WindowStyle Hidden -Wait
+Start-Process "$(scoop prefix v2raya)\v2rayaWin.exe" -ArgumentList $startOptions -WorkingDirectory "$env:TEMP" -WindowStyle Hidden
 
